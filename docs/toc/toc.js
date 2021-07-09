d1e2fcc4416c25cf789e205df044b55dce19e373
Mika Braginsky <mika.br@gmail.com>
2021-07-09 14:14:01 -0400

    fix toc path

--- docs/toc/toc.js
+++ docs/toc/toc.js
@@ -1,11 +1,23 @@
+const hover = (target, element, property) => window.addEventListener('load', () => {
+	const [hide, show] = [0, element.getBoundingClientRect()[property]]
+	.map(value => () => element.style[property] = value + 'px')
+
+	target.addEventListener('mouseenter', show);
+	target.addEventListener('mouseleave', hide);
+
+	hide();
+})
+
+const title = 'Experimental Methods in the Behavioral and Cognitive Sciences';
+
 const header = document.createElement('header');
 header.id = 'TOC';
 
-const book = document.createElement('a');
-book.className = 'book-title'
-book.textContent = document.querySelector('.title').textContent;
-book.href = 'index.html';
-header.append(book);
+const bookTitle = document.createElement('a');
+bookTitle.className = 'book-title';
+bookTitle.textContent = title;
+bookTitle.href = 'index.html';
+header.append(bookTitle);
 
 const nav = document.createElement('nav');
 header.append(nav);
@@ -15,42 +27,48 @@ TOC.querySelectorAll('li')
 	const [title] = textContent.match(/(?<=\s).+/g);
 
 	if (classList.contains('part')) {
+		const [, first, rest] = title.match(/([A-z]+)(.*)/);
+
 		const partContainer = document.createElement('div');
 		partContainer.className = 'part-container';
 		nav.append(partContainer);
 
-		const partTitle = document.createElement('a');
+		const partTitle = document.createElement('div');
 		partTitle.className = 'part-title';
 		partContainer.append(partTitle);
 
-		const first = document.createElement('div');
-		first.className = 'first';
-		partTitle.append(first);
+		const partTitleFirst = document.createElement('div');
+		partTitleFirst.className = 'part-title-first';
+		partTitleFirst.textContent = first;
+		partTitle.append(partTitleFirst);
 
-		const restContainer = document.createElement('div');
-		restContainer.className = 'rest-container';
-		partTitle.append(restContainer);
-
-		const rest = document.createElement('div');
-		rest.className = 'rest';
-		restContainer.append(rest);
-
-		[, first.textContent, rest.textContent] = title.match(/([A-z]+)(.*)/);
-
-		const dropdownContainer = document.createElement('div');
-		dropdownContainer.className = 'dropdown-container';
-		partContainer.append(dropdownContainer);
+		const partTitleRest = document.createElement('div');
+		partTitleRest.className = 'part-title-rest';
+		partTitleRest.textContent = rest;
+		partTitle.append(partTitleRest);
 
 		const dropdown = document.createElement('div');
 		dropdown.className = 'dropdown';
-		dropdownContainer.append(dropdown);
+		partContainer.append(dropdown);
+
+		hover(partContainer, partTitleRest, 'width');
 	} else {
+		const partContainer = nav.lastChild;
+		const dropdown = partContainer.lastChild;
+
 		const chapterTitle = document.createElement('a');
 		chapterTitle.className = 'chapter-title';
 		chapterTitle.textContent = title;
 		chapterTitle.href = href;
-		nav.querySelector('div:last-child .dropdown-container .dropdown').append(chapterTitle);
+		dropdown.append(chapterTitle);
+
+		hover(partContainer, chapterTitle, 'width');
+		hover(partContainer, chapterTitle, 'height');
 	}
 });
 
 TOC.replaceWith(header);
+
+window.addEventListener('load', () => {
+	header.style.height = header.getBoundingClientRect().height + 'px';
+});
