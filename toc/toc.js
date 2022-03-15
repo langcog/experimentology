383e43d9e63c28ce86391efcd6f5e2ee16e25a2d
mikabr <mika.br@gmail.com>
2022-03-15 19:07:53 +0000

    mikabr published a site update

---
+++ toc/toc.js
@@ -0,0 +1,91 @@
+const hover = (target, element, property) => window.addEventListener('load', () => {
+  let isShown = false;
+  const size = element.getBoundingClientRect()[property];
+  const hide = () => { 
+	  element.style[property] = '0px';
+    isShown = false;
+  }
+  const show = () => {
+    element.style[property] = size + 'px';
+    isShown = true;
+  }
+  const toggle = () => {isShown ? hide() : show()};
+
+	target.addEventListener('mouseenter', show);
+	target.addEventListener('mouseleave', hide);
+
+	// On mobile, let the user toggle the submenus individual on tap
+	// since hover doesn't work
+	target.addEventListener('touchstart', toggle)
+
+	hide();
+})
+
+const title = 'Experimentology: An Open Science Approach to Experimental Psychology Methods';
+
+const header = document.createElement('header');
+header.id = 'TOC';
+
+const bookTitle = document.createElement('a');
+bookTitle.className = 'book-title';
+bookTitle.textContent = title;
+bookTitle.href = 'index.html';
+header.append(bookTitle);
+
+const nav = document.createElement('nav');
+header.append(nav);
+
+TOC.querySelectorAll('li')
+.forEach(({classList, firstChild: {textContent, href}}) => {
+//	const [title] = textContent.match(/(?<=\s).+/g);
+// this is a hack to split PART titles to create the title as their first word
+    const [title] = textContent.match(/\s.+/g); // safari fix
+
+  // | classList.contains('appendix') 
+  // add this to parse appendix parts
+	if (classList.contains('part')) {
+		const [, first, rest] = title.match(/([A-z]+)(.*)/);
+
+		const partContainer = document.createElement('div');
+		partContainer.className = 'part-container';
+		nav.append(partContainer);
+
+		const partTitle = document.createElement('div');
+		partTitle.className = 'part-title';
+		partContainer.append(partTitle);
+
+		const partTitleFirst = document.createElement('div');
+		partTitleFirst.className = 'part-title-first';
+		partTitleFirst.textContent = first;
+		partTitle.append(partTitleFirst);
+
+		const partTitleRest = document.createElement('div');
+		partTitleRest.className = 'part-title-rest';
+		partTitleRest.textContent = rest;
+		partTitle.append(partTitleRest);
+
+		const dropdown = document.createElement('div');
+		dropdown.className = 'dropdown';
+		partContainer.append(dropdown);
+
+		hover(partContainer, partTitleRest, 'width');
+	} else {
+		const partContainer = nav.lastChild;
+		const dropdown = partContainer.lastChild;
+
+		const chapterTitle = document.createElement('a');
+		chapterTitle.className = 'chapter-title';
+		chapterTitle.textContent = title;
+		chapterTitle.href = href;
+		dropdown.append(chapterTitle);
+
+		hover(partContainer, chapterTitle, 'width');
+		hover(partContainer, chapterTitle, 'height');
+	}
+});
+
+TOC.replaceWith(header);
+
+window.addEventListener('load', () => {
+	header.style.height = header.getBoundingClientRect().height + 'px';
+});
