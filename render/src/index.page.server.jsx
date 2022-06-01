a2be13134cbebaef8d9bc3fd0ab38428456f8f13
natalie <hi@natalie.computer>
2022-06-01 12:52:11 -0400

    fix #46

--- render/src/index.page.server.jsx
+++ render/src/index.page.server.jsx
@@ -7,27 +7,6 @@ import parse                     from 'html-react-parser'
 import renderToString            from 'preact-render-to-string'
 import * as library              from 'preact'
 
-const descendants = node => [
-	node.children,
-	node.children?.map(descendants),
-].flat(Infinity).filter(Boolean)
-
-const textContent = node => descendants(node)
-.filter(({ type }) => type == 'text')
-.map(({ data }) => data)
-.join('')
-
-const replace = node => {
-	if (node?.attribs?.id == 'TOC') return <TOC/>;
-
-	if (
-		node.name == 'p' && descendants(node)
-		.some(node => node.name == 'button' && descendants(node)
-			.some(node => node.data.match(/^(Next|Previous)$/))
-		)
-	) return <></>;
-}
-
 export const prerender = () => routes
 
 export const onBeforeRender = async ({ url }) => {
@@ -40,6 +19,35 @@ export const onBeforeRender = async ({ url }) => {
 	return { pageContext: { html } };
 }
 
-export const render = ({ html }) => dangerouslySkipEscape(
-	'<!DOCTYPE html>' + renderToString(parse(html, { library, replace }))
-)
+export const render = ({ url, html }) => {
+	const descendants = node => [
+		node.children,
+		node.children?.map(descendants),
+	].flat(Infinity).filter(Boolean)
+
+	const textContent = node => descendants(node)
+	.filter(({ type }) => type == 'text')
+	.map(({ data }) => data)
+	.join('')
+
+	const isPageButton = node => node.name == 'p' && descendants(node)
+	.some(node => node.name == 'button' && descendants(node)
+		.some(node => node.data.match(/^(Next|Previous)$/))
+	)
+
+	const isLastPage = url == routes.at(-1);
+
+	const replace = node => {
+		if (node?.attribs?.id == 'TOC') return <TOC/>;
+
+		if (isPageButton(node)) return <></>;
+
+		if (isLastPage && node?.attribs?.id == 'refs') return <></>;
+	}
+
+	html = parse(html, { library, replace });
+	html = renderToString(html)
+	html = dangerouslySkipEscape('<!DOCTYPE html>' + html);
+
+	return html;
+}
