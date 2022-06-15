6047dc8e207a3cd88f4ea51126a1cf2416356e20
natalie <hi@natalie.computer>
2022-06-15 14:55:35 -0400

    use vite plugin for copying static assets

--- render/vite.config.js
+++ render/vite.config.js
@@ -1,9 +1,24 @@
-import preact from '@preact/preset-vite'
-import reload from 'vite-plugin-full-reload'
-import ssr    from 'vite-plugin-ssr/plugin'
+import preact             from '@preact/preset-vite'
+import reload             from 'vite-plugin-full-reload'
+import ssr                from 'vite-plugin-ssr/plugin'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default {
 	plugins : [
+		viteStaticCopy({
+			flatten : false,
+			targets: [
+				{
+					src : [
+						'r/experimentology_files',
+						'r/images',
+						'r/libs',
+						'r/reference-keys.txt',
+					],
+					dest : '.',
+				},
+			],
+		}),
 		preact(),
 		reload([ 'src/**' ]),
 		ssr(),
