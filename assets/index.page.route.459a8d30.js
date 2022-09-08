dba5b2be7ae6fc8ccad0cf8e853d221cf9cf44b9
mcfrank <mcfrank@stanford.edu>
2022-09-08 18:24:59 +0000

    Deploying to gh-pages from @ langcog/experimentology@f10b2475748ee5c097137e60df92da8c88b9d855 🚀

--- assets/index.page.route.459a8d30.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./src/index.page.client.jsx.afd0ce61.js";var t=({url:e})=>r.includes(e.replace(/\.html$/,"").replace(/index$/,""));export{t as default};
---
+++ assets/index.page.route.7f58ccf9.js
@@ -0,0 +1,1 @@
+import{r}from"./src/index.page.client.jsx.dbd931e0.js";var t=({url:e})=>r.includes(e.replace(/\.html$/,"").replace(/index$/,""));export{t as default};
