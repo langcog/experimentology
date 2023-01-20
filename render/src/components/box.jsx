63c56cd1b65f654b66477b1efabd012ecea6f652
natalie <hi@natalie.computer>
2023-01-19 19:35:47 -0500

    encapsulate solarized color scheme

--- render/src/components/box.jsx
+++ render/src/components/box.jsx
@@ -15,25 +15,6 @@ import {
 	faPersonFallingBurst,
 } from '@fortawesome/free-solid-svg-icons'
 
-const theme = {
-	base03  : '#002b36',
-	base02  : '#073642',
-	base01  : '#586e75',
-	base00  : '#657b83',
-	base0   : '#839496',
-	base1   : '#93a1a1',
-	base2   : '#eee8d5',
-	base3   : '#fdf6e3',
-	yellow  : '#b58900',
-	orange  : '#cb4b16',
-	red     : '#dc322f',
-	magenta : '#d33682',
-	violet  : '#6c71c4',
-	blue    : '#268bd2',
-	cyan    : '#2aa198',
-	green   : '#859900',
-};
-
 const boxes = {
 	learning_goals         : { color: 'red',     icon: faAppleWhole,          open: true,  preview: false },
 	case_study             : { color: 'blue',    icon: faMicroscope,          open: true,  preview: false },
@@ -58,7 +39,7 @@ const Box = ({ type, children }) => {
 	);
 
 	return (
-		<div class={ style.box } style={{ '--color': theme[color] }}>
+		<div class={ style.box } style={{ '--color': `var(--${color})` }}>
 			<Collapsible
 				className={ preview && style.preview }
 				trigger={ trigger }
