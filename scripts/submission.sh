25c9dcf89da90c0ead484de93b3988a39c283b08
Mika Braginsky <mika.br@gmail.com>
2025-04-04 08:50:59 -0400

    copyright page tweaks

--- scripts/submission.sh
+++ scripts/submission.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # for f in *_files/*pdf/*png images/*/*png; do out=submit/${f/.png/.jpg}; dir=$(dirname $out); echo $f; mkdir -p $dir; magick $f -colorspace cmyk -quality 100 $out; done
-for f in *_files/*pdf/*png images/*/*png; do out=submit/${f/.png/.jpg}; dir=$(dirname $out); echo $f; mkdir -p $dir; magick $f -profile "../USWebCoatedSWOP.icc" -quality 100 $out; done
+# for f in *_files/*pdf/*png images/*/*png; do out=submit/${f/.png/.jpg}; dir=$(dirname $out); echo $f; mkdir -p $dir; magick $f -profile "../USWebCoatedSWOP.icc" -quality 100 $out; done
 
 cp Experimentology_fixed.tex submit/Experimentology_fixed.tex
-sed s/png/jpg/g submit/Experimentology_fixed.tex > submit/Experimentology_jpgs.tex
 
-# tinytex::lualatex("Experimentology_jpgs.tex", pdf_file = "Experimentology.pdf")
+# cd submit/
+sed s/png/jpg/g Experimentology_fixed.tex > Experimentology.tex
+rm Experimentology_fixed.tex 
+Rscript -e 'tinytex::lualatex("Experimentology.tex", pdf_file = "Experimentology.pdf")'
